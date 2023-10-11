81e7b47130856db28ad49d3cf61b2b38687dedfa
emmanuelmeena91 <emmanuel.meena@moringaschool.com>
2023-10-11 13:34:54 +0300

    complete

--- src/components/AgentDashboard.js
+++ src/components/AgentDashboard.js
@@ -31,24 +31,24 @@ function AgentDashboard() {
   }
 
   return (
-<div className="agent-dashboard bg-gray-100 min-h-screen flex flex-col items-center justify-center p-4">
-  <div className="dashboard-links mb-6">
-    <Link to="/home" className="text-blue-500 hover:underline mr-4">Home</Link>
-    <Link to="/createhouse" className="text-blue-500 hover:underline mr-4">Create House</Link>
-    <Link to="/updatehouse" className="text-blue-500 hover:underline mr-4">Update House</Link>
-    <Link to="/deletehouse" className="text-blue-500 hover:underline mr-4">Delete House</Link>
-    <Link to={`/agents/${id}`} className="text-blue-500 hover:underline mr-4">Agent Dashboard</Link>
-    <button onClick={handleLogout} className="text-red-500 hover:underline">Logout</button>
-  </div>
-  <div className="bg-white p-8 rounded shadow-md w-96">
-    <h2 className="text-2xl font-semibold mb-4">Agent Details</h2>
-    <p className="text-gray-600 mb-2">Name: {agent.name}</p>
-    <p className="text-gray-600 mb-2">Email: {agent.email}</p>
-    <p className="text-gray-600 mb-4">Phone Number: {agent.phonebook}</p>
-    {/* Links to other pages */}
-    {/* Display other agent details as needed */}
-  </div>
+    <div className="agent-dashboard bg-gray-100 min-h-screen flex flex-col items-center justify-center p-4" style={{ backgroundImage: 'url("https://www.landlordvision.co.uk/blog/wp-content/uploads/2020/10/87916602_s.jpg")', backgroundSize: "cover", backgroundPosition: "center" }}>
+     <div className="dashboard-links mb-6">
+  <Link to="/home" className="text-black text-lg bg-white hover:bg-blue-500 hover:underline px-4 py-2 rounded-lg mr-4">Home</Link>
+  <Link to="/createhouse" className="text-black text-lg bg-white hover:bg-blue-500 hover:underline px-4 py-2 rounded-lg mr-4">Create House</Link>
+  <Link to="/updatehouse" className="text-black text-lg bg-white hover:bg-blue-500 hover:underline px-4 py-2 rounded-lg mr-4">Update House</Link>
+  <Link to="/deletehouse" className="text-black text-lg bg-white hover:bg-blue-500 hover:underline px-4 py-2 rounded-lg mr-4">Delete House</Link>
+  <Link to={`/agents/${id}`} className="text-black text-lg bg-white hover:bg-blue-500 hover:underline px-4 py-2 rounded-lg mr-4">Agent Dashboard</Link>
+  <button onClick={handleLogout} className="text-black text-lg bg-red-400 hover:bg-red-600 hover:underline px-4 py-2 rounded-lg">Logout</button>
 </div>
+      <div className="bg-white p-8 rounded shadow-md w-96">
+        <h2 className="text-2xl text-blue-500 font-semibold mb-4">Agent Details</h2>
+        <p className="text-blue-600  mb-2">Name: {agent.name}</p>
+        <p className="text-blue-600 mb-2">Email: {agent.email}</p>
+        <p className="text-blue-600 mb-4">Phone Number: {agent.phonebook}</p>
+        {/* Links to other pages */}
+        {/* Display other agent details as needed */}
+      </div>
+    </div>
   );
 }
 
--- src/components/HouseDetails.js
+++ src/components/HouseDetails.js
@@ -32,30 +32,30 @@ function HouseDetails() {
   }
 
   return (
-<div className="house-details-container bg-gray-100 p-8 flex flex-col md:flex-row items-center justify-center">
-  <div className="house-details-text mr-0 md:mr-12 mb-8 md:mb-0 text-center md:text-left">
-    {/* Display house details */}
-    <h2 className="text-3xl font-semibold mb-4">{house.title}</h2>
-    <p className="text-lg mb-2">Description {house.description}</p>
-    <p className="text-lg mb-2">Price: Ksh {house.price}</p>
-    <p className="text-lg mb-2">Size: {house.size}</p>
-    <p className="text-lg mb-2">Area: {house.city}</p>
-    <p className="text-lg mb-2">County: {house.county}</p>
-    <p className="text-lg mb-2">Bedrooms: {house.bedrooms}</p>
-    <p className="text-lg mb-2">Bathrooms: {house.bathrooms}</p>
-    <p className="text-lg mb-2">Agent_ID: {house.agent_id}</p>
+    <div className="house-details-container bg-gray-100 p-8 flex flex-col md:flex-row items-center justify-center">
+      <div className="house-details-text mr-0 md:mr-12 mb-8 md:mb-0 text-center md:text-left">
+        {/* Display house details */}
+        <h2 className="text-3xl font-semibold mb-4">{house.title}</h2>
+        <p className="text-lg mb-2">Description: {house.description}</p>
+        <p className="text-lg mb-2">Price: Ksh {house.price}</p>
+        <p className="text-lg mb-2">Size: {house.size}</p>
+        <p className="text-lg mb-2">Area: {house.city}</p>
+        <p className="text-lg mb-2">County: {house.county}</p>
+        <p className="text-lg mb-2">Bedrooms: {house.bedrooms}</p>
+        <p className="text-lg mb-2">Bathrooms: {house.bathrooms}</p>
+        <p className="text-lg mb-2">Agent ID: {house.agent_id}</p>
 
-    {/* Display other house details as needed */}
-    
-    {/* Display agent details */}
-    <h2 className="text-2xl font-semibold mt-6 mb-2">Agent Details</h2>
-    <p className="text-lg mb-2">Name: {agent.name}</p>
-    <p className="text-lg mb-2">Email: {agent.email}</p>
-  </div>
-  <div className="house-details-image">
-    <img src={house.image_paths} alt={house.title} className="w-64 h-64 object-cover rounded-md" />
-  </div>
-</div>
+        {/* Display other house details as needed */}
+        
+        {/* Display agent details */}
+        <h2 className="text-2xl font-semibold text-blue-500 mt-6 mb-2">Agent Details</h2>
+        <p className="text-lg mb-2">Name: {agent.name}</p>
+        <p className="text-lg mb-2">Email: {agent.email}</p>
+      </div>
+      <div className="house-details-image">
+        <img src={house.image_paths} alt={house.title} className="w-96 h-96 object-cover rounded-md" />
+      </div>
+    </div>
   );
 }
 
